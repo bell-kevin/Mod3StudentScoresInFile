9f2071e2c7297740c07919b2f114ab8276e46315
Kevin Bell <bellKevin@pm.me>
2022-09-22 20:34:12 -0600

    Add files via upload

---
+++ mod3StudentScoresInFile/src/mod3studentscoresinfile/Mod3StudentScoresInFile.java
@@ -0,0 +1,24 @@
+/** ****************************************************************************
+ * Mod3StudentScoresInFile.java
+ * Kevin Bell
+ *
+ * Input read from files, calculate & display lowest, highest, & average score
+ **************************************************************************** */
+package mod3studentscoresinfile;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.*;
+import java.util.*;
+
+public class Mod3StudentScoresInFile {
+
+    public static void main(String[] args) {
+        String filename = "gradebook.txt";
+        Path path;
+        System.out.println("Module 3 Student Scores by Kevin Bell");
+        path = Paths.get(filename);
+        Scanner computerKeyboardInput = new Scanner(System.in);
+        
+    } // end main method  
+} // end Mod3StudentScoresInFile class
